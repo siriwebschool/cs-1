7bc7826c177182d567c4704ddc873e97dca4f951
hamdasalam <107979765+hamdasalam@users.noreply.github.com>
2024-09-21 13:04:39 +0000

    ranges

---
+++ add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int sum(int a, int b){
+ //return a+b;
+}
+
+int main() {
+
+    int a = 2;
+    int b = 3;
+    int c =  sum(b,a);
+    printf("sum is %d",c);
+    return 0;
+}
