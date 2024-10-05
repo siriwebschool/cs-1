c69f6fd47ca59fff816d7e57d0772f92deeb7309
Siri <8502281+siri404-gh@users.noreply.github.com>
2024-10-05 14:21:41 +0100

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -20,3 +20,24 @@ int main()
         fahr = fahr + step;
     }
 }
+
+// using float
+
+float convert (float a) {
+    return (5.0/9.0) * (a-32);
+}
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    lower = 0; /* lower limit of temperature scale */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = convert(fahr);
+        printf("%f\t%f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
