78c0c58e16241c1c6f413234b8e7f416bdfa0586
hamda salam <hamdasalam@gmail.com>
2024-11-01 15:36:49 +0000

    Sum of two numbers

--- 2-add.c
+++ 2-add.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int sum(int a, int b){
+    return a+b;
+}
+
 int main() {
-int a,b;
-printf("enter a");
-scanf("%d", &a);
-printf("enter b");
-scanf("%d" , &b);
-int sum = a + b;
-printf("sum is %d", sum);
-return 0;
+
+    int a = 2;
+    int b = 3;
+    int c =  sum(b,a);
+    printf("sum is %d",c);
+    return 0;
 }
