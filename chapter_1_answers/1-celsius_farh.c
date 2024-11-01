dcf9c47aad75eefa76e28b18db004b3a356c7538
hamda salam <hamdasalam@gmail.com>
2024-11-01 18:43:35 +0000

    Program 1 renamed

---
+++ chapter_1_answers/1-celsius_farh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main() {
+    float celsius, fahrenheit;
+    int lower = 0;    // Lower limit of temperature table
+    int upper = 100;  // Upper limit of temperature table
+    int step = 10;    // Step size
+
+
+    printf("Celsius to Fahrenheit Conversion Table\n");
+ 
+   
+    celsius = lower;
+    while (celsius <= upper) {
+        fahrenheit = (celsius * 9.0 / 5.0) + 32;  // Conversion formula
+        printf("%6.1f °C  =  %6.1f °F\n", celsius, fahrenheit);
+        celsius = celsius + step;
+    }
+
+
+    return 0;
+}
