86878074ee3768f8a93788b95e2eb7bd1f8758ff
hamda salam <hamdasalam@gmail.com>
2024-11-01 17:32:59 +0000

    Program - Call by Value

--- call_by_value.c
+++ call_by_value.c
@@ -1,16 +1,15 @@
-
 #include <stdio.h>
 
 // Function to attempt to change the value of a
-void modifyValue(int *p) {
-    *p = 10;  // Change the local copy of a inside the function
+void modifyValue(int a) {
+    a = 10;  // Change the local copy of a inside the function
 }
 
 int main() {
     int a = 5;  // Initialize variable a
     printf("Before modifyValue: a = %d\n", a);  // Print value of a before function call
 
-    modifyValue(&a);  // Call the function with a as argument
+    modifyValue(a);  // Call the function with a as argument
 
     printf("After modifyValue: a = %d\n", a);  // Print value of a after function call
     return 0;
